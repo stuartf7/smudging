2419ec1ff7280183882696d80dad38af36a5c2c8
Stuart Fleming <contact@stuartfleming.co.uk>
2013-02-06 00:36:06 +0000

    Adding filters and setup

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo "You are about to install the smudge. Please make sure that all"
+echo "your changes have been commited."
+echo 
+
+read -r -p "Are you sure? [Y/n] " -n 1
+if [[ ! ($REPLY == "Y" || $REPLY == "y") ]]
+then
+	echo
+	echo "No changes made."
+	exit 0
+fi
+
+
+echo
+echo
+echo "Installing repository filters..."
+git config filter.cleaner.clean "~/.clean"
+git config filter.cleaner.smudge "~/.smudge"
+
+
+cp .smudge ~/.smudge
+echo "Installed smudge filter into " ~/.smudge
+cp .clean ~/.clean
+echo "Installed clean filter into " ~/.clean
+
+echo
+
+find . -name "*.config" | while read FILENAME;
+do
+	echo "SMUDGING: " $FILENAME
+	git rm $FILENAME
+	git checkout $FILENAME
+done
+
+echo ""
+echo ""
+echo "NOTE:"
+echo "If you have a non default setup then you can change"
+echo "the values that are applied to during the smudge by"
+echo "editing " ~/.smudge " with your own values."
+exit 0
